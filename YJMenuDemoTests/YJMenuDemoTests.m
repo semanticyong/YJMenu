f5b8751c130e742b200be43cb16bea932e08f68c
yong <yong@yong-imac.corp.good.com>
2014-10-27 17:47:53 +0800

    Initial Commit

---
+++ YJMenuDemoTests/YJMenuDemoTests.m
@@ -0,0 +1,40 @@
+//
+//  YJMenuDemoTests.m
+//  YJMenuDemoTests
+//
+//  Created by yong on 10/27/14.
+//  Copyright (c) 2014 yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface YJMenuDemoTests : XCTestCase
+
+@end
+
+@implementation YJMenuDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
